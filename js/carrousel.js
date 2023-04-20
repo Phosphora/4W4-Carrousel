1408701913fbbdd44449c9a62ac81463b78ec499
Winnie Li <winnieli535@gmail.com>
2023-04-19 21:58:00 -0400

    s11c2 Ajout radio bouton et Ajout écouteur sur elm de la galerie

--- js/carrousel.js
+++ js/carrousel.js
@@ -5,18 +5,23 @@
     let carrousel = document.querySelector(".carrousel");
     let carrousel__x = document.querySelector(".carrousel__x");
     let carrousel__figure = document.querySelector(".carrousel__figure");
+    let carrousel__form = document.querySelector(".carrousel__form");
 
     let galerie = document.querySelector(".galerie");
     let galerie__img = galerie.querySelectorAll("img");
     console.log(galerie__img);
 
-    bouton.addEventListener('mousedown', function(){
+    let index = 0;
+    let ancien_index = -1;
+    let position = 0; // Permet d'indexer les images de la galerie et
+
+    bouton.addEventListener('mousedown', function() {
         console.log('Ouvrir la boîte modale.');
         carrousel.classList.add('carrousel--activer');
         ajouter_img_dans_carrousel();
     })
 
-    carrousel__x.addEventListener('mousedown', function(){
+    carrousel__x.addEventListener('mousedown', function() {
         console.log('Fermer la boîte modale.');
         carrousel.classList.remove('carrousel--activer');
     })
@@ -28,11 +33,56 @@
 
     function ajouter_img_dans_carrousel() {
         for (const elm of galerie__img) {
-            // console.log(elm.getAttribute('src'));
-            let img = document.createElement('img');
-            img.setAttribute('src', elm.getAttribute('src'));
-            console.log(img.getAttribute('src'));
-            carrousel__figure.appendChild(img);
+            elm.dataset.index = position;
+            elm.addEventListener('mousedown', function() {
+                index = this.dataset.index;
+                afficher_image(index);
+                console.log(index);
+            });
+            creation_img_carrousel(elm);
+            creation_radio_carrousel();
         }
+    }
+
+    function creation_img_carrousel(elm) {
+        // console.log(elm.getAttribute('src'));
+        let img = document.createElement('img');
+        // img.setAttribute('src', elm.getAttribute('src'));
+        img.src = elm.src;
+        img.classList.add('carrousel__img');
+        // console.log(img.getAttribute('src'));
+        carrousel__figure.appendChild(img);
+    }
+
+    /**
+     * Création d'un radio bouton
+     */
+
+    function creation_radio_carrousel() {
+        let rad = document.createElement('input');
+        rad.setAttribute('type', 'radio');
+        rad.setAttribute('name', 'carrousel__rad');
+        rad.classList.add('carrousel__rad');
+        rad.dataset.index = position;
+        position = position + 1 // Incrémentation de 1
+        // position += 1
+        // position++
+        carrousel__form.appendChild(rad);
+
+        rad.addEventListener('mousedown', function(){
+            console.log(this.dataset.index);
+            index = this.dataset.index;
+            afficher_image(index);
+        });
+    }
+
+    function afficher_image(index) {
+        if (ancien_index != -1) {
+            // carrousel__figure.children[ancien_index].style.opacity = 0;
+            carrousel__figure.children[ancien_index].classList.remove('carrousel__img--activer');
+        }
+        // carrousel__figure.children[index].style.opacity = 1;
+        carrousel__figure.children[index].classList.add('carrousel__img--activer');
+        ancien_index = index;
     }
 })()
